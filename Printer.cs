999acbe8f513d30441c1289195864bc686d5ba47
muhammad naufal <muhammadnaufal321@students.amikom.ac.id>
2023-06-12 20:09:40 +0700

    pertemuan9

---
+++ Printer.cs
@@ -0,0 +1,36 @@
+﻿using PrinterApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Pilih printer:");
+        Console.WriteLine("1. Canon");
+        Console.WriteLine("2. Epson");
+        Console.WriteLine("3. LaserJet");
+        Console.Write("Nomor printer [1...3]: ");
+        int choice = int.Parse(Console.ReadLine());
+
+        string brand;
+        switch (choice)
+        {
+            case 1:
+                brand = "Canon Printing........";
+                break;
+            case 2:
+                brand = "Epson printing........";
+                break;
+            case 3:
+                brand = "Laserjet Printing........";
+                break;
+            default:
+                Console.WriteLine("tidak ada bg");
+                return;
+        }
+
+        Printer printer = new Printer(brand);
+        printer.Print();
+
+        Console.ReadLine();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PrinterApp
+{
+    class Printer
+    {
+        private string brand;
+
+        public Printer(string brand)
+        {
+            this.brand = brand;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Printer " + brand);
+        }
+    }
+}
